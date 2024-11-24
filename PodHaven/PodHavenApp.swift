592ec196250c201db35f9bbf62806af47dd056cc
Justin Bishop <jubishop@gmail.com>
2024-11-23 16:06:53 -0800

    Initial commit

---
+++ PodHaven/PodHavenApp.swift
@@ -0,0 +1,12 @@
+// Copyright Justin Bishop, 2024 
+
+import SwiftUI
+
+@main
+struct PodHavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PodHavenTests/PodHavenTests.swift
@@ -0,0 +1,12 @@
+// Copyright Justin Bishop, 2024 
+
+import Testing
+@testable import PodHaven
+
+struct PodHavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
