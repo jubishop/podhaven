d0454d4f801b0e173dfa96145509d5f94b0a9ff8
Justin Bishop <jubishop@gmail.com>
2024-12-13 18:22:25 -0800

    add Identified Array fetcher

--- PodHaven/Database/Utility/GRDB.swift
+++ PodHaven/Database/Utility/GRDB.swift
@@ -2,9 +2,18 @@
 
 import Foundation
 import GRDB
+import IdentifiedCollections
 
 extension DerivableRequest {
   func shuffled() -> Self {
     order(sql: "RANDOM()")
+  }
+}
+
+extension FetchRequest where RowDecoder: FetchableRecord & Identifiable {
+  public func fetchIdentifiedArray(_ db: Database)
+    throws -> IdentifiedArrayOf<RowDecoder>
+  {
+    try IdentifiedArray(fetchCursor(db))
   }
 }
