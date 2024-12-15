b963f2a2533dcc6f0554f8cd87ed3049774b6bf0
Justin Bishop <jubishop@gmail.com>
2024-12-15 09:34:02 -0800

    setting up to manage the MediaPlayer

---
+++ PodHaven/Shared/MPTransport.swift
@@ -0,0 +1,10 @@
+// Copyright Justin Bishop, 2024
+
+import Foundation
+import MediaPlayer
+
+@globalActor
+final actor MPTransport {
+  static let shared = MPTransport()
+
+}
--- PodHaven/Views/Now Playing/Models/PlayBarViewModel.swift
+++ PodHaven/Views/Now Playing/Models/PlayBarViewModel.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
   private var _sliderValue: Double = 0
   var sliderValue: Double {
-    get {
-      isDragging ? _sliderValue : PlayState.shared.currentTime.seconds
-    }
+    get { isDragging ? _sliderValue : PlayState.shared.currentTime.seconds }
     set {
       self._sliderValue = newValue
       Task { @PlayManager in
