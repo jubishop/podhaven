cb70dd346f83b61a3c712481905dad4ebbe9bf4b
Justin Bishop <jubishop@gmail.com>
2024-12-24 15:38:24 -0800

    use both assets for preview loading

--- PodHaven/Preview Content/Helpers.swift
+++ PodHaven/Preview Content/Helpers.swift
@@ -2,23 +2,21 @@
 
 import Foundation
 
-
 enum Helpers {
-  static func loadSeries() async throws -> PodcastSeries? {
+  private static let seriesFiles = ["pod_save_america", "land_of_the_giants"]
+
+  static func loadSeries(fileName: String = seriesFiles.randomElement()!)
+    async throws -> PodcastSeries?
+  {
     let parseResult = await PodcastFeed.parse(
-      Bundle.main.url(
-        forResource: "pod_save_america",
-        withExtension: "rss"
-      )!
+      Bundle.main.url(forResource: fileName, withExtension: "rss")!
     )
     guard case .success(let feedResult) = parseResult,
       let unsavedPodcast = feedResult.toUnsavedPodcast(
         oldFeedURL: URL(string: "https://jubi.com")!,
         oldTitle: "Pod Save America"
       )
-    else {
-      throw FeedError.failedParse("Could not load pod_save_america")
-    }
+    else { throw FeedError.failedParse("Could not load \(fileName)") }
     return try await Repo.shared.insertSeries(
       unsavedPodcast,
       unsavedEpisodes: feedResult.items.map {
