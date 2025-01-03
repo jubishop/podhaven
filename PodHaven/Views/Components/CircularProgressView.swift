860633ed73411f3d330e0e4b137d3931ad97f9ae
Justin Bishop <jubishop@gmail.com>
2025-01-03 09:40:31 -0800

    cooler and cleaner pie charting

--- PodHaven/Views/Components/CircularProgressView.swift
+++ PodHaven/Views/Components/CircularProgressView.swift
@@ -1,64 +1,42 @@
 // Copyright Justin Bishop, 2025
 
+import Charts
 import OrderedCollections
 import SwiftUI
 
 struct CircularProgressView: View {
-  struct PieWedge: Shape {
-    var startAngle: Angle
-    var endAngle: Angle
-
-    func path(in rect: CGRect) -> Path {
-      var path = Path()
-      let center = CGPoint(x: rect.midX, y: rect.midY)
-      let radius = min(rect.width, rect.height) / 2
-      path.move(to: center)
-      path.addArc(
-        center: center,
-        radius: radius,
-        startAngle: startAngle,
-        endAngle: endAngle,
-        clockwise: false
-      )
-      path.closeSubpath()
-      return path
-    }
-  }
-
   private let totalAmount: Double
   private let colorAmounts: OrderedDictionary<Color, Double>
-  private var totalColorAmount: Double {
-    colorAmounts.values.reduce(0, +)
-  }
+  private let innerRadius: MarkDimension
+  private let angularInset: CGFloat?
+  private var totalColorAmount: Double { colorAmounts.values.reduce(0, +) }
   init(
-    totalAmount: Double = 100,
-    colorAmounts: OrderedDictionary<Color, Double>
+    totalAmount: Double = 1,
+    colorAmounts: OrderedDictionary<Color, Double>,
+    innerRadius: MarkDimension = .ratio(0.5),
+    angularInset: CGFloat? = 2
   ) {
     self.totalAmount = totalAmount
     self.colorAmounts = colorAmounts
+    self.innerRadius = innerRadius
+    self.angularInset = angularInset
   }
 
   var body: some View {
-    let (colorAngles, _) = colorAmounts.reduce(
-      into: (OrderedDictionary<Color, (Double, Double)>(), Double(-90))
-    ) { (dict_angle, element) in
-      let computedAmount =
-        totalColorAmount > totalAmount
-        ? element.value * totalAmount / totalColorAmount : element.value
-      let newAngle = (360 * computedAmount / totalAmount)
-      dict_angle.0[element.key] = (dict_angle.1, dict_angle.1 + newAngle)
-      dict_angle.1 += newAngle
-    }
-    ZStack {
-      ForEach(Array(colorAngles.keys), id: \.self) { color in
-        if let (startAngle, endAngle) = colorAngles[color] {
-          PieWedge(
-            startAngle: Angle.degrees(startAngle),
-            endAngle: Angle.degrees(endAngle)
-          )
-          .fill(color.gradient)
-        }
-      }.overlay(Circle().stroke(.primary))
+    Chart {
+      ForEach(Array(colorAmounts.keys), id: \.self) { color in
+        let amount = colorAmounts[color] ?? 0
+        SectorMark(
+          angle: .value("Value", amount),
+          innerRadius: innerRadius,
+          angularInset: angularInset
+        )
+        .foregroundStyle(color.gradient)
+      }
+      if totalAmount > totalColorAmount {
+        SectorMark(angle: .value("Value", totalAmount - totalColorAmount))
+          .foregroundStyle(.opacity(0))
+      }
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .aspectRatio(1, contentMode: .fit)
