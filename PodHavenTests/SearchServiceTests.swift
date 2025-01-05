ad97f9e4d233a2d8e1efbb8eb30c375beabeb939
Justin Bishop <jubishop@gmail.com>
2025-01-04 18:39:29 -0800

    properly mocking search results for testing

--- PodHavenTests/SearchServiceTests.swift
+++ PodHavenTests/SearchServiceTests.swift
@@ -7,16 +7,27 @@ import Testing
 
 @Suite("of SearchService tests")
 actor SearchServiceTests {
+  static private let baseURLString = "https://api.podcastindex.org/api/1.0"
+
+  private let session: NetworkingMock
   private let service: SearchService
 
   init() {
-    service = SearchService(session: URLSession.shared)
+    session = NetworkingMock()
+    service = SearchService(session: session)
   }
 
   @Test("basic search query")
   func testBasicSearchQuery() async throws {
-    let result = try await service.searchByTerm("hard fork")
+    let searchTerm = "hard fork"
+    let data = try Data(
+      contentsOf: Bundle.main.url(forResource: "hardfork", withExtension: "json")!
+    )
+    await session.set(
+      URL(string: Self.baseURLString + "/search/byterm?q=\(searchTerm)")!,
+      .data(data)
+    )
+    let result = try await service.searchByTerm(searchTerm)
     #expect(result.feeds.first!.title == "Hard Fork")
   }
 }
-
