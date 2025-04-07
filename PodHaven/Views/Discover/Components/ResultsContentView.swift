a23ed770fd900789d15207f714c4351097b32794
Justin Bishop <jubishop@gmail.com>
2025-04-07 10:12:28 -0700

    add a preview for the new ContentView component

--- PodHaven/Views/Discover/Components/ResultsContentView.swift
+++ PodHaven/Views/Discover/Components/ResultsContentView.swift
@@ -5,7 +5,7 @@ import SwiftUI
 struct ResultsContentView<SearchedPodcastType: SearchedPodcast>: View {
   private let viewModel: ResultsViewModel
   private let createSearchedPodcast: (String, UnsavedPodcast) -> SearchedPodcastType
-  
+
   init(
     viewModel: ResultsViewModel,
     createSearchedPodcast: @escaping (String, UnsavedPodcast) -> SearchedPodcastType
@@ -13,7 +13,7 @@ struct ResultsContentView<SearchedPodcastType: SearchedPodcast>: View {
     self.viewModel = viewModel
     self.createSearchedPodcast = createSearchedPodcast
   }
-  
+
   var body: some View {
     VStack {
       if viewModel.result != nil {
@@ -45,5 +45,30 @@ struct ResultsContentView<SearchedPodcastType: SearchedPodcast>: View {
         Spacer()
       }
     }
+  }
+}
+
+#Preview {
+  @Previewable @State var viewModel: ResultsViewModel?
+
+  NavigationStack {
+    if let viewModel = viewModel {
+      ResultsContentView<SearchedPodcastByTerm>(
+        viewModel: viewModel
+      ) { searchText, unsavedPodcast in
+        SearchedPodcastByTerm(
+          searchedText: searchText,
+          unsavedPodcast: unsavedPodcast
+        )
+      }
+      .navigationTitle("🔍 Preview")
+    }
+  }
+  .preview()
+  .task {
+    let termResult = try! await PreviewHelpers.loadTermResult()
+    viewModel = ResultsViewModel(
+      searchResult: TermSearchResult(searchText: "Preview Test", termResult: termResult)
+    )
   }
 }
