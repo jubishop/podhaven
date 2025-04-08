7c160e0e075c014921fcb805612a6c2259f0ac83
Justin Bishop <jubishop@gmail.com>
2025-04-07 23:00:46 -0700

    remove totally unused file whew

--- PodHaven/Discover/SearchModels/SearchedEpisodeByPerson.swift
+++
@@ -1,8 +0,0 @@
-// Copyright Justin Bishop, 2025
-
-import Foundation
-
-struct SearchedEpisodeByPerson: Sendable, Hashable {
-  let searchedText: String
-  let unsavedPodcastEpisode: UnsavedPodcastEpisode
-}
