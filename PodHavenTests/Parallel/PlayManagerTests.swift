1c9e6ffa08f99a17f9f1e73ecd0af5ec875fd0e9
Justin Bishop <jubishop@gmail.com>
2025-05-08 14:53:13 -0700

    move play manager test back into parallel

--- PodHavenTests/Parallel/PlayManagerTests.swift
+++ PodHavenTests/Parallel/PlayManagerTests.swift
@@ -1,11 +1,12 @@
 // Copyright Justin Bishop, 2025
 
+import FactoryTesting
 import Foundation
 import Testing
 
 @testable import PodHaven
 
-@Suite("of PlayManager tests", .serialized)
+@Suite("of PlayManager tests", .container)
 struct PlayManagerTests {
   @Test("example")
   func example() throws {
