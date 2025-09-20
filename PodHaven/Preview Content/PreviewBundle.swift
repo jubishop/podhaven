3b7c52156efb235d811061f549659266160cb25d
Justin Bishop <jubishop@gmail.com>
2025-09-19 17:42:24 -0700

    ✨ refactor(preview): Add image caching and improve asset loading
    This commit introduces caching for loaded images and their data within the `PreviewBundle` to optimize preview loading times.
    
    The `PreviewBundle` struct now includes two thread-safe dictionaries: `cachedImages` and `cachedImageData` to store `UIImage` and `Data` respectively.
    
    When an image or its data is requested, the code first checks if the asset is already present in the cache. If found, it's returned directly, avoiding redundant loading and processing. Otherwise, the asset is loaded, cached, and then returned.
    
    Additionally, a private helper function `buildNamespacedName` has been added to centralize the logic for constructing asset names with their respective folder paths, improving code readability and maintainability.

--- PodHaven/Preview Content/PreviewBundle.swift
+++ PodHaven/Preview Content/PreviewBundle.swift
@@ -15,9 +15,12 @@ enum ImageFolder: String {
   case EpisodeThumbnails
 }
 
-enum PreviewBundle {
+struct PreviewBundle {
+  static let cachedImages = ThreadSafe<[String: UIImage]>([:])
+  static let cachedImageData = ThreadSafe<[String: Data]>([:])
+
   static func loadAsset(named name: String, in folder: AssetFolder) -> Data {
-    let namespacedName = "\(folder.rawValue)/\(name)"
+    let namespacedName = buildNamespacedName(for: name, in: folder)
 
     guard let dataAsset = NSDataAsset(name: namespacedName, bundle: Bundle.main)
     else { fatalError("Could not load data asset: \(namespacedName) from main bundle") }
@@ -26,21 +29,32 @@ enum PreviewBundle {
   }
 
   static func loadImage(named name: String, in folder: ImageFolder) -> UIImage {
-    let namespacedName = "\(folder.rawValue)/\(name)"
+    let namespacedName = buildNamespacedName(for: name, in: folder)
+    if let cachedImage = cachedImages[namespacedName] { return cachedImage }
 
     guard let uiImage = UIImage(named: namespacedName, in: Bundle.main, compatibleWith: nil)
     else { fatalError("Could not load image: \(namespacedName) from main bundle") }
 
+    cachedImages[namespacedName] = uiImage
     return uiImage
   }
 
   static func loadImageData(named name: String, in folder: ImageFolder) -> Data {
-    let uiImage = loadImage(named: name, in: folder)
+    let namespacedName = buildNamespacedName(for: name, in: folder)
+    if let cachedImageData = cachedImageData[namespacedName] { return cachedImageData }
 
+    let uiImage = loadImage(named: name, in: folder)
     guard let data = uiImage.pngData()
     else { fatalError("Could not convert image to PNG data: \(name)") }
 
+    cachedImageData[namespacedName] = data
     return data
+  }
+
+  private static func buildNamespacedName(for name: String, in folder: any RawRepresentable)
+    -> String
+  {
+    "\(folder.rawValue)/\(name)"
   }
 }
 #endif
