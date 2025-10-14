cf1a0757166c5006411e78cc0078c3c3f483ef81
Justin Bishop <jubishop@gmail.com>
2025-10-13 18:25:43 -0700

    ✨ refactor(ui): simplify ItemGrid initialization and remove redundant ID generic
    The `ItemGrid` struct has been simplified by removing the explicit `idKeyPath` generic parameter and its associated initialization. This was redundant as the `ForEach` loop in SwiftUI can automatically infer the `Identifiable` conformance from the collection's elements.
    
    The `ForEach(items, id: idKeyPath)` has been replaced with `ForEach(items)`, relying on the `Data.Element: Identifiable` constraint. This reduces the complexity of the `ItemGrid` struct and its initialization, making it more concise and idiomatic within SwiftUI. The associated extension that provided a convenience initializer for `Identifiable` elements has also been removed as it's no longer necessary.

--- PodHaven/Views/Components/ItemGrid.swift
+++ PodHaven/Views/Components/ItemGrid.swift
@@ -5,39 +5,27 @@ import GRDB
 import IdentifiedCollections
 import SwiftUI
 
-struct ItemGrid<Data: RandomAccessCollection, ID: Hashable, Content: View>: View {
+struct ItemGrid<Data: RandomAccessCollection, Content: View>: View
+where Data.Element: Identifiable {
   private let items: Data
-  private let idKeyPath: KeyPath<Data.Element, ID>
   private let minimumGridSize: CGFloat
   private let content: (Data.Element) -> Content
 
   init(
     items: Data,
-    id: KeyPath<Data.Element, ID>,
     minimumGridSize: CGFloat? = nil,
     @ViewBuilder content: @escaping (Data.Element) -> Content
   ) {
     self.items = items
-    idKeyPath = id
     self.minimumGridSize = minimumGridSize ?? CGFloat(100)
     self.content = content
   }
 
   var body: some View {
     LazyVGrid(columns: [GridItem(.adaptive(minimum: minimumGridSize))]) {
-      ForEach(items, id: idKeyPath) { item in
+      ForEach(items) { item in
         content(item)
       }
     }
-  }
-}
-
-extension ItemGrid where Data.Element: Identifiable, ID == Data.Element.ID {
-  init(
-    items: Data,
-    minimumGridSize: CGFloat? = nil,
-    @ViewBuilder content: @escaping (Data.Element) -> Content
-  ) {
-    self.init(items: items, id: \.id, minimumGridSize: minimumGridSize, content: content)
   }
 }
