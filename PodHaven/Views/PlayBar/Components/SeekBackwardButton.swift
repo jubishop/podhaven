62cbeaf3ad309127908b34f3d182efd19bb85211
Justin Bishop <jubishop@gmail.com>
2026-09-11 19:48:27 -0700

    Preserve Play Bar dependency scope across SwiftUI callbacks (#671) (#673)

--- PodHaven/Views/PlayBar/Components/SeekBackwardButton.swift
+++ PodHaven/Views/PlayBar/Components/SeekBackwardButton.swift
@@ -4,12 +4,13 @@ import FactoryKit
 import SwiftUI
 
 struct SeekBackwardButton: View {
+  @Injected(\.userSettings) private var userSettings
   @State private var animationTrigger = false
 
   let action: () -> Void
 
   var body: some View {
-    AppIcon.seekBackward(Int(Container.shared.userSettings().skipBackwardInterval))
+    AppIcon.seekBackward(Int(userSettings.skipBackwardInterval))
       .imageButton {
         animationTrigger.toggle()
         action()
